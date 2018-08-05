a27c38b34c719a5902edd7a0d905ee692fb6aad2
shayanjavadi <shayanjavadi1375@gmail.com>
2018-08-05 11:44:54 -0500

    netlify form fix attempt 4 ;((

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,24 +11,25 @@ const ContactForm = () => (
     <div className="container">
       <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <div className="field half first">
+          <label htmlFor="name">
+            Name*
+          </label>
+          <Input type="text" name="name" id="name" placeholder="Your Name" />
+        </div>
+        <div className="field half">
+          <label htmlFor="email">
+            Email*
+          </label>
+          <Input type="text" name="email" id="email" placeholder="example@domain.com" />
+        </div>
+        <div className="field">
+          <label htmlFor="message">
+            Message*
+          </label>
+          <Input name="message" id="message" rows="6" isTextArea />
+        </div>
+        <Button type="submit" text="Submit" raised />
       </form>
     </div>
   </section>
