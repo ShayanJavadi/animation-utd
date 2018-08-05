9f63a0966b2c7e680464f0670bc4e1a99f872c9e
shayanjavadi <shayanjavadi1375@gmail.com>
2018-08-05 11:36:17 -0500

    netlify form fix attempt 2

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,27 +9,25 @@ const ContactForm = () => (
   <section className={PACKAGE_NAME}>
     <SectionHeader text="Lorem Ipsum Kitty Pasta" />
     <div className="container">
-      <form action="/success" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-        <Input type="hidden" name="bot-field" />
-        <div className="field half first">
-          <label htmlFor="name">
-            Name*
-          </label>
-          <Input type="text" name="name" id="name" placeholder="Your Name" />
-        </div>
-        <div className="field half">
-          <label htmlFor="email">
-            Email*
-          </label>
-          <Input type="text" name="email" id="email" placeholder="example@domain.com" />
-        </div>
-        <div className="field">
-          <label htmlFor="message">
-            Message*
-          </label>
-          <Input name="message" id="message" rows="6" isTextArea />
-        </div>
-        <Button text="Submit" raised />
+      <form name="contact" method="POST" netlify>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
     </div>
   </section>
