b52aae3c10fd853b1bfa3411bf3769fd77865836
Kevin Chen <ningchen227@gmail.com>
2019-02-06 11:20:11 -0600

    Fixed favicon

--- src/common/HTMLContent.js
+++ src/common/HTMLContent.js
@@ -2,6 +2,4 @@ import React from "react";
 
 const PACKAGE_NAME = "HTML-Content-component";
 
-export default ({ html, classNames = "" }) => 
-<link rel="shortcut icon" type="image/ico" href="https://raw.githubusercontent.com/ShayanJavadi/animation-utd/master/assets/images/favicon.ico" />
-<div dangerouslySetInnerHTML={{ __html: html }} className={classNames} />;
+export default ({ html, classNames = "" }) => <div dangerouslySetInnerHTML={{ __html: html }} className={classNames} />;
